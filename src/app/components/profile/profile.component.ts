e1670d255b5cee7b29501d77e6602c87dd6173a1
Milad Raeisi <miladsoft@yahoo.com>
2024-09-15 01:28:45 +0400

    Start updating profile

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -2,6 +2,7 @@ import { TextFieldModule } from '@angular/cdk/text-field';
 import { NgClass } from '@angular/common';
 import {
     ChangeDetectionStrategy,
+    ChangeDetectorRef,
     Component,
     ViewEncapsulation,
 } from '@angular/core';
@@ -12,8 +13,11 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatTooltipModule } from '@angular/material/tooltip';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { AngorCardComponent } from '@angor/components/card';
+import { AngorConfigService } from '@angor/services/config';
+import { NostrService } from 'app/services/nostr.service';
+import { SignerService } from 'app/services/signer.service';
 
 @Component({
     selector: 'profile',
@@ -36,8 +40,41 @@ import { AngorCardComponent } from '@angor/components/card';
     ],
 })
 export class ProfileComponent {
-    /**
-     * Constructor
-     */
-    constructor() {}
+    user: any;
+    isLoading: boolean = true;
+    errorMessage: string | null = null;
+    metadata: any;
+
+    constructor(
+        private _changeDetectorRef: ChangeDetectorRef,
+        private _router: Router,
+        private _angorConfigService: AngorConfigService,
+        private _nostrService: NostrService,
+        private _signerService: SignerService
+    ) { }
+    ngOnInit(): void {
+        this.loadUserProfile();
+    }
+    private async loadUserProfile(): Promise<void> {
+        this.isLoading = true;
+        this.errorMessage = null;
+        const publicKey = this._signerService.getPublicKey();
+
+        if (!publicKey) {
+            this.errorMessage = 'No public key found. Please log in again.';
+            this.isLoading = false;
+            return;
+        }
+
+        try {
+            const metadata = await this._nostrService.fetchMetadata(publicKey);
+            this.metadata = metadata;
+        } catch (error) {
+            console.error('Failed to load profile data:', error);
+            this.errorMessage = 'Failed to load profile data. Please try again later.';
+        } finally {
+            this.isLoading = false;
+            this._changeDetectorRef.markForCheck();  // Trigger change detection to update the view
+        }
+    }
 }
