fe49d79e608915c35b06b86b46f1d1ec2d360389
Milad Raeisi <miladsoft@yahoo.com>
2024-09-17 20:41:04 +0400

    Add pwa

--- src/main.ts
+++ src/main.ts
@@ -5,3 +5,8 @@ import { appConfig } from 'app/app.config';
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
     console.error(err)
 );
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/ngsw-worker.js').then(() => {
+      console.log('Service Worker Registered');
+    });
+  }
