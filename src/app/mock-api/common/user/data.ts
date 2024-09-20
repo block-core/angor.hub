b597312fc415c228355ac21e501a86343aa75f5c
Milad Raeisi <miladsoft@yahoo.com>
2024-09-21 00:33:20 +0400

    Remove mockApi

--- src/app/mock-api/common/user/data.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable */
-export const user = {
-    id: 'cfaad35d-07a3-4447-a6c3-d8c3d54fd5df',
-    name: 'Username',
-    email: 'username@angor.io',
-    avatar: 'images/avatars/avatar-placeholder.png',
-    status: 'online',
-};
